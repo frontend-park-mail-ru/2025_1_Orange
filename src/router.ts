f3927aefcd1b9d450a080f488c459a40199fc24f
hello-larin <larinalesha@mail.ru>
2025-04-14 13:27:37 +0300

    добавил /

--- src/router.ts
+++ src/router.ts
@@ -54,6 +54,7 @@ class Router {
             logger.info(`Маршрут ${parsed} найден`);
             this.#routes[parsed]();
         } else {
+            this.go('/catalog')
             logger.warn(`Маршрут ${parsed} не найден`);
         }
     };
